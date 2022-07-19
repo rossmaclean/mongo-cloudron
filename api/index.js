cd65bcca44be70a4ede1a699a60809ebe6b96681
Ross MacLean <ross@rossmac.co.uk>
2022-07-19 09:58:11 +0100

    Remove unneeded log.

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ app.get('/', (req, res) => {
     MongoClient.connect('mongodb://localhost:27017', {connectTimeoutMS: 2000, socketTimeoutMS: 2000, waitQueueTimeoutMS: 2000, serverSelectionTimeoutMS: 2000, wtimeoutMS: 2000}, function (err, db) {
         if (err) {
             console.log("Health Check - Mongo is down");
-            console.log(err)
             res.sendStatus(500);
         } else {
             console.log("Health Check - Mongo is up");
